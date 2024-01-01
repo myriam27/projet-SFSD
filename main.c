a0d8c34b89cb20fa5539744fbc2affc4ff9e0a65
Lina <lina.gali@yahoo.com>
2024-01-01 01:39:35 +0100

    second commit i added some functions

--- main.c
+++ main.c
@@ -30,16 +30,76 @@ struct TypeBloc
     int suivant;
 };
 
-
-typedef struct entete {
-    int nbr-pre; //le num du premier bloc
-    int nbr-ins; // le num de l'element qu'on veut insérer
-    int nbr-sup; //le num de l'element  a suprimé
-    int nbr-der; //le num du dernier bloc
-    int pos-vide-lb; // la premiere position vide dans le dernier bloc
-    int nb-bloc // nombre total des blocs
+typedef struct entete entete;
+struct entete
+{
+    int nbrpre; //le num du premier bloc
+    int nbrins; // le num de l'element qu'on veut insérer
+    int nbrsup; //le num de l'element  a suprimé
+    int nbrder; //le num du dernier bloc
+    int posvidelb; // la premiere position vide dans le dernier bloc
+    int nb_bloc // nombre total des blocs
     unsigned long long last_key; // WHY THIS ????
+};
+// la declaration du fichier 
+typedef struct Fichier Fichier;
+struct Fichier
+{
+    entete head; // champs de l'entete 
+    FILE *file 
+};
+
+int Entete(int i, Fichier* fichier  )
+{
+    switch (i)
+    {
+    case 1 : return fichier->entete.nbrpre;
+    case 2 : return fichier->entete.nbins;
+    case 3 : return fichier->entete.nbrsup;
+    case 4:  return fichier->entete.nbrder;
+    case 5: return fichier->entete.nb_bloc;
+    case 6: return fichier->entete.posvidelb;
+    default:
+        printf(" ERROR !! ");
+        return 0;
+    }
+
 }
+void Aff_Entete( Fichier *fichier , int i,int valeur)
+{
+    switch (i)
+    {
+        case 1: {
+            fichier->entete.nbrpre = valeur;
+            break;
+        }
+        case 2: {
+            fichier->entete.nbins = valeur;
+            break;
+        }
+        case 3: {
+            fichier->entete.nbsup = valeur;
+            break;
+        }
+        case 4: {
+            fichier->entete.nbrder= valeur;
+            break;
+        }
+        case 5: {
+            fichier->entete.nb_bloc = valeur;
+            break;
+        }
+        case 6: {
+            fichier->entete.posvidelb = valeur;
+            break;
+        }
+        default {
+            printf(" ERROR !!\n  ");
+            break;
+        }
+    }
+}
+
 void ouvrir_f(FILE* f,char* nomf,char mode){
     if((mode=='A')||(mode=='a'))
         *f=fopen(nomf,"rb+");
